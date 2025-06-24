46f49e2120cdb208f012a5eec459e73031f4bedb
pefuz <fuzi.peter83@googlemail.com>
2025-06-24 18:31:06 +0200

    time

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -27,7 +27,7 @@ class TestHootel(object):
         self.browser.set_window_size(991, 600)
         hamburger_btn = self.browser.find_element(By.XPATH, "//span[@class='navbar-toggler-icon']")
         hamburger_btn.click=()
-        time.sleep(1)
+        time.sleep(2)
         login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
         login_btn.click()
 
